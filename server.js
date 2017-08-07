468227087f6e785e402f1fa43ee54cf635515068
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-07 17:21:48 -0400

    created SQL database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const data = require('./data')
-const pg = require('pg-promise')
+const pgPromise = require('pg-promise')()
+
+const database = pgPromise({database: "t1000"})
 
 //mark would use console.log data hear to make sure we have it!
 console.log(data)
@@ -21,16 +23,17 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', (request, response) => {
-	//sending content to the browser from javascript
-	response.render('index', data)
+  database.any('SELECT * FROM "t1000" ').then(robodata => {
+	response.render('index', {users: robotdata})
 })
-app.get('/info/:id', (request, response) => {
-	const requestId = parseInt(request.params.id)
-
-	const foundUser = data.users.find(user => user.id === requestId)
-
-	response.render('info', foundUser)
 })
+// app.get('/info/:id', (request, response) => {
+// 	const requestId = parseInt(request.params.id)
+//
+// 	const foundUser = data.users.find(user => user.id === requestId)
+//
+// 	response.render('info', foundUser)
+// })
 /*
 app.get('users/:id', (request, response) => {
 	response.render('users', data)
