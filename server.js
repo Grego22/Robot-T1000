17b79cdc1f73cce72884fb3093794bef79abea69
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-07 18:16:16 -0400

    rendering images

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const data = require('./data')
 const pgPromise = require('pg-promise')()
-
 const database = pgPromise({database: "t1000"})
+const mustacheExpress = require('mustache-express')
 
 //mark would use console.log data hear to make sure we have it!
-console.log(data)
 
-const mustacheExpress = require('mustache-express')
+
 
 //teach our app to use public for all public files
 app.use(express.static('public'))
@@ -23,23 +22,23 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', (request, response) => {
-  database.any('SELECT * FROM "t1000" ').then(robodata => {
+  database.any('SELECT * FROM "t1000" ').then(robotdata => {
 	response.render('index', {users: robotdata})
+  })
 })
+app.get('/info/:id', (request, response) => {
+	const requestId = parseInt(request.params.id)
+
+	const foundUser = data.users.find(user => user.id === requestId)
+
+	response.render('info', foundUser)
 })
-// app.get('/info/:id', (request, response) => {
-// 	const requestId = parseInt(request.params.id)
-//
-// 	const foundUser = data.users.find(user => user.id === requestId)
-//
-// 	response.render('info', foundUser)
-// })
 /*
 app.get('users/:id', (request, response) => {
 	response.render('users', data)
   const requestId = request.params.id
 })
 */
-app.listen(7777, function() {
+app.listen(7778, function() {
 	console.log('Looking good Billy Ray!!!')
 })
